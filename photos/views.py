e967564d8cbdcf5355c160a864400726422d1083
Olugbenga Alao <gasttronomy@gmail.com>
2021-05-21 13:43:27 +0100

    Created a file upload form

---
+++ photos/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from photos.models import Photo
+
+
+# Create your views here.
+
+def index(request):
+    photos = Photo.objects.all()
+
+    context = {
+        "photos": photos
+    }
+
+    return render(request, 'photos/index.html', context)
